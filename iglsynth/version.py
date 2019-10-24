5a4779c908df74536fd5071313dab47f1c16f412
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2019-10-24 19:04:04 -0400

    Version

--- iglsynth/version.py
+++ iglsynth/version.py
@@ -2,7 +2,7 @@
 # Versioning scheme is Major.Minor.Micro
 # Minor and Major are changed when there are breaking changes or stable versions are released.
 _major = 0
-_minor = 1
+_minor = 2
 _micro = 0
 __version__ = f"{_major}.{_minor}.{_micro}"
 
