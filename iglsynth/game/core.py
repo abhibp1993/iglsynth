cf8068b8bd8915421cba25a10dd381ff04ffa078
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2019-11-14 14:10:10 -0500

    core.Action class added, with decorator

--- iglsynth/game/core.py
+++ iglsynth/game/core.py
@@ -4,16 +4,11 @@ iglsynth: core.py
 License goes here...
 """
 
-import abc
-from iglsynth.util.graph import *
-
+from inspect import signature
+from typing import Callable
 
 CONCURRENT = "Concurrent"
 TURN_BASED = "Turn-based"
-
-
-class Kripke(abc.ABC):
-    pass
 
 
 class Player(object):
@@ -21,4 +16,24 @@ class Player(object):
 
 
 class Action(object):
-    pass
+    def __init__(self, name=None, func=None):
+        assert isinstance(func, Callable), f"Input parameter func must be a function, got {func.__class__}."
+        assert len(signature(func).parameters) == 1, f"Function 'func' must take exactly one parameter."
+
+        self._name = name
+        self._func = func
+
+    def __repr__(self):
+        return f"Action(name={self._name})"
+
+    def __call__(self, v):
+        return self._func(v)
+
+
+def action(func):
+    """
+    Decorator for creating atomic propositions.
+    """
+    a = Action(name=func.__name__, func=func)
+    return a
+
