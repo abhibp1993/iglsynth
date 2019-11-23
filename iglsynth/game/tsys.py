ceb347056a446f971d268863a39a7763d644b73b
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2019-11-23 00:05:50 -0500

    TSys.__init__ and TSys.Vertex, TSys.Edge classes refactored.

--- iglsynth/game/tsys.py
+++ iglsynth/game/tsys.py
@@ -7,109 +7,134 @@ CONCURRENT = "Concurrent"
 
 
 class TSys(Kripke):
+    """
+    A graph representing a Transition System (TS).
+    A TS is a Kripke structure where all edges are labeled with actions.
+
+    :param kind: (:data:`TURN_BASED` or :data:`CONCURRENT`) Whether the transition system is turn-based or concurrent.
+    :param alphabet: (:class:`Alphabet`) A set of atomic propositions defined over the Kripke structure.
+    :param vtype: (class) Class representing vertex objects.
+    :param etype: (class) Class representing edge objects.
+    :param graph: (:class:`Graph`) Copy constructor. Copies the input graph into new Kripke object.
+    :param file: (str) Name of file (with absolute path) from which to load the Kripke graph.
+
+    .. note:: Kripke structure class is defined as a placeholder. It may be used to define structures like
+              :class:`TSys`.
+    """
 
     # ------------------------------------------------------------------------------------------------------------------
     # PUBLIC CLASSES
     # ------------------------------------------------------------------------------------------------------------------
     class Vertex(Kripke.Vertex):
-        def __init__(self, turn=None):
+        """
+        Class for representing a vertex of a transition system.
+
+        - Vertex has a name, which is a unique id.
+        - Optionally, a vertex may have a turn associated with it. When ``turn`` is not ``None``,
+          then the vertex represents a TURN-BASED transition system. Otherwise, the vertex
+          represents a CONCURRENT transition system.
+
+        """
+        # --------------------------------------------------------------------------------------------------------------
+        # INTERNAL FUNCTIONS
+        # --------------------------------------------------------------------------------------------------------------
+        def __init__(self, name, turn=None):
+            assert isinstance(name, str), f"Parameter 'name' must be a string. " \
+                f"Received name={name} of type(name)={type(name)}."
             assert isinstance(turn, int) or turn is None, \
                 f"Parameter 'turn' must be an integer, greater equal 0. Received {turn}."
 
+            self._name = name
             self._turn = turn
 
+        def __hash__(self):
+            return self._name.__hash__()
+
+        def __eq__(self, other):
+            assert type(other) == self.__class__
+            return self._name == other.name
+
+        def __repr__(self):
+            return f"{self.__class__.__name__}(name={self.name}, turn={self.turn})"
+
+        # --------------------------------------------------------------------------------------------------------------
+        # PUBLIC PROPERTIES
+        # --------------------------------------------------------------------------------------------------------------
         @property
         def turn(self):
+            """ Returns the player whose turn it is at the vertex. """
             return self._turn
 
-    class Edge(Kripke.Edge):
-        ACTIONS = set()
+        @property
+        def name(self):
+            """ Returns the name associated with the vertex. """
+            return self._name
 
-        def __init__(self, u, v, act=None):
+    class Edge(Kripke.Edge):
+        """
+        Class for representing a edge of graph.
+
+        :param u: (:class:`TSys.Vertex`) Source vertex of edge.
+        :param v: (:class:`TSys.Vertex`) Target vertex of edge.
+        :param action: (:class:`Action` ) Action label of the edge.
+        """
+
+        # --------------------------------------------------------------------------------------------------------------
+        # INTERNAL FUNCTIONS
+        # --------------------------------------------------------------------------------------------------------------
+        def __init__(self, u, v, action=None):
             super(TSys.Edge, self).__init__(u, v)
-            self._act = act
-            self.__class__.ACTIONS.add(act)
+            self._action = action
 
         def __hash__(self):
-            return (self._source, self._target, self._act).__hash__()
+            return (self._source, self._target, self._action).__hash__()
 
         def __repr__(self):
             return f"{self.__class__.__name__}." \
-                f"ConcurrentEdge(u={self._source}, v={self._target}, a1={self._act})"
+                f"{self.__class__.__name__}(u={self._source}, v={self._target}, a1={self._action})"
 
         def __eq__(self, other: 'TSys.Edge'):
-            return self._source == other._source and self._target == other._target and self._act == other._act
+            assert type(other) == self.__class__
+            return self._source == other._source and self._target == other._target and self._action == other._action
 
+        # --------------------------------------------------------------------------------------------------------------
+        # PUBLIC PROPERTIES
+        # --------------------------------------------------------------------------------------------------------------
         @property
         def action(self):
-            return self._act
+            """ Returns the action associated with the edge. """
+            return self._action
 
     # ------------------------------------------------------------------------------------------------------------------
     # INTERNAL METHODS
     # ------------------------------------------------------------------------------------------------------------------
-    def __init__(self, kind, field=None, p1=None, p2=None,
-                 actions=None, alphabet=None, label_func=None,
-                 vtype=None, etype=None, graph=None, file=None):
+    def __init__(self, kind, alphabet=None, vtype=None, etype=None, graph=None, file=None):
 
         # Validate input arguments
         assert kind in [TURN_BASED, CONCURRENT], f"A TSys kind must be either {TURN_BASED} or {CONCURRENT}."
-        assert isinstance(field, Field) or field is None
-        assert isinstance(p1, Player) or p1 is None
-        assert isinstance(p2, Player) or p2 is None
-        assert isinstance(actions, Iterable) or actions is None
-        assert all([isinstance(p, AP) for p in alphabet]) if alphabet is not None else True
-        assert isinstance(label_func, Callable) or label_func is None  # TODO: Change this to signature validation.
-
-        # Set Vertex and Edge type for this object based on kind of transition system
-        if kind == TURN_BASED:
-            assert issubclass(vtype, TSys.TurnBasedVertex) or vtype is None
-            self.Vertex = self.TurnBasedVertex if vtype is None else vtype
-
-            assert issubclass(etype, self.TurnBasedEdge) or etype is None
-            self.Edge = self.TurnBasedEdge if etype is None else etype
-
-        else:   # kind == CONCURRENT
-            assert isinstance(vtype, TSys.ConcurrentVertex) or vtype is None
-            self.Vertex = self.ConcurrentVertex if vtype is None else vtype
-
-            assert isinstance(etype, self.ConcurrentEdge) or etype is None
-            self.Edge = self.ConcurrentEdge if etype is None else etype
-
-        # Default values
-        vtype = TSys.Vertex if vtype is None else vtype
-        etype = TSys.Edge if etype is None else etype
-        actions = set() if actions is None else actions
-        alphabet = set() if alphabet is None else alphabet
 
         # Base class constructor
-        super(TSys, self).__init__(alphabet=alphabet, label_func=label_func,
-                                   vtype=vtype, etype=etype, graph=graph, file=file)
+        super(TSys, self).__init__(alphabet=alphabet, vtype=vtype, etype=etype, graph=graph, file=file)
 
         # Defining parameters
-        self._field = field
-        self._p1 = p1
-        self._p2 = p2
-        self._actions = actions
         self._kind = kind
+        self._p1_actions = set()
+        self._p2_actions = set()
 
     # ------------------------------------------------------------------------------------------------------------------
     # PROPERTIES
     # ------------------------------------------------------------------------------------------------------------------
     @property
-    def field(self):
-        return self._field
+    def p1_action(self):
+        return self._p1_actions
 
     @property
-    def p1(self):
-        return self._p1
-
-    @property
-    def p2(self):
-        return self._p2
+    def p2_actions(self):
+        return self._p2_actions
 
     @property
     def actions(self):
-        return self.Edge.ACTIONS
+        return set.union(self._p1_actions, self._p2_actions)
 
     @property
     def kind(self):
