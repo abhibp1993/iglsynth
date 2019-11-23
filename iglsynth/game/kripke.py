a0a9df1ed01be010ec751d2f296a5b6e2263bff8
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2019-11-23 16:53:07 -0500

    Refactored Kripke class

--- iglsynth/game/kripke.py
+++ iglsynth/game/kripke.py
@@ -17,8 +17,8 @@ class Kripke(Graph):
     :param graph: (:class:`Graph`) Copy constructor. Copies the input graph into new Kripke object.
     :param file: (str) Name of file (with absolute path) from which to load the Kripke graph.
 
-    .. note:: Kripke structure class is defined as a placeholder. It may be used to define structures like
-              :class:`TSys`.
+    .. note:: Kripke structure class is defined as a placeholder. It may be used to define structures similar to
+              :class:`TSys`. However, presently not much thought has been given to it.
     """
 
     # ------------------------------------------------------------------------------------------------------------------
@@ -27,7 +27,8 @@ class Kripke(Graph):
     def __init__(self, alphabet=None, vtype=None, etype=None, graph=None, file=None):
 
         # Validate input data-types
-        assert isinstance(alphabet, Alphabet) or alphabet is None
+        assert isinstance(alphabet, Alphabet) or alphabet is None, \
+            f"Parameter alphabet must be an Alphabet object. Received type(alphabet)={type(alphabet)}."
 
         # Base class constructor
         super(Kripke, self).__init__(vtype=vtype, etype=etype, graph=graph, file=file)
@@ -38,24 +39,31 @@ class Kripke(Graph):
 
     # ------------------------------------------------------------------------------------------------------------------
     # PROPERTIES
-    # ------------------------------------------------------------------------------------------------------------------\
+    # ------------------------------------------------------------------------------------------------------------------
     @property
     def is_left_total(self):
         raise NotImplementedError(f"{self}.is_left_total property is not yet implemented.")       # pragma: no cover
 
     @property
     def alphabet(self):
+        """ Returns the alphabet associated with the Kripke structure. """
         return self._alphabet
 
     @property
     def init_state(self):
+        """ Returns the initial state of the initial state(s) of the Kripke structure as a ``set``. """
         return self._init_st
 
     # ------------------------------------------------------------------------------------------------------------------
     # PUBLIC METHODS
     # ------------------------------------------------------------------------------------------------------------------
-
     def initialize(self, init_st):
+        """
+        Defines the initial states of the Kripke structure.
+
+        :param init_st: (:class:`Kripke.Vertex` or set of :class:`Kripke.Vertex` objects) Initial state(s) of the
+            Kripke structure.
+        """
         if isinstance(init_st, Iterable):
             assert all(isinstance(st, Kripke.Vertex) for st in init_st)
             self._init_st = set(init_st)
@@ -68,5 +76,5 @@ class Kripke(Graph):
                 raise AssertionError(f"{self.__class__.__name__} is already initialized. "
                                      f"Reinitialization is not allowed.")
 
-            self._init_st = init_st
+            self._init_st = {init_st}
 
