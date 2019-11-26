ad5fe4b8d79736788de3cf835bab6f34770e9d4c
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2019-11-26 10:58:58 -0500

    Defined Solver Interface

---
+++ iglsynth/solver/core.py
@@ -0,0 +1,17 @@
+from iglsynth.game.game import *
+
+
+class ISolver(object):
+    def __init__(self, game):
+        assert isinstance(game, Game)
+        self._game = game
+
+    def _validate_game(self):
+        pass
+
+    def configure(self):
+        pass
+
+    def solve(self):
+        pass
+
